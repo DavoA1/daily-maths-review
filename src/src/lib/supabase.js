23e72693c4b0a05c5cbe8734e23e27494e10ec09
DavoA1 <davo.arthur@gmail.com>
2026-04-26 14:36:38 +1000

    Delete src/src directory

--- src/src/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const SUPABASE_URL = 'https://jrjuugkfeblkaysfolza.supabase.co'
-const SUPABASE_ANON_KEY = 'sb_publishable_u7s72zKB01-F8jgiZRRWgQ_iekcWgcJ'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
